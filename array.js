22a76fbc3415a8554b2c2471ee49ca2d07a600d4
rijwanulalam <rijwanul007@gmail.com>
2023-11-05 18:14:44 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,69 @@
+// var friendsAge = [23, 26, 20, 32, 28];
+// friendsAge.unshift(21);
+// console.log(friendsAge.length);
+// console.log(friendsAge);
+// var position = friendsAge.indexOf(20);
+// console.log(position);
+
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+
+// var index = fruits.indexOf('Banana');
+// console.log(index);
+
+// fruits[1] = 'Mango';
+
+// fruits.pop();
+// console.log(fruits);
+// fruits.push('WaterMelon');
+// console.log(fruits);
+// var myMarks = 85;
+// var tomsMark = 66;
+// var janesMarks = 95;
+// var petersMarks = 56;
+// var johnsMarks = 40;
+
+// if(tomsMark >= 80){
+//     console.log('A Grade');
+// }
+// else if(tomsMark >= 60){
+//     console.log('B Grade');
+// }
+// else if(tomsMark >= 50){
+//     console.log('C Grade');
+// }
+// else if(tomsMark >= 40){
+//     console.log('D Grade');
+// }
+// else if(tomsMark <= 39){
+//     console.log('Fail');
+// }
+// else{
+//     console.log('Did not appear in the exam');
+// }
+
+
+// var num1 = 13;
+// var num2 = 79;
+// var num3 = 85;
+
+// if (num1 > (num2 && num3)){
+//     console.log(num1 +" " + "is the maximum number");
+// }
+// else if(num2 > (num1 && num3)){
+//     console.log(num2 +" " + "is the maximum number")
+// }
+// else{
+//     console.log(num3 +" " + "is the maximum number")
+// }
+
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+
+if(side1 === side2 || side1 === side3){
+    console.log("triangle is isoceles")
+}
+else{
+    console.log("triangle is not isoceles")
+}
---
+++ jaan.js
@@ -0,0 +1,17 @@
+var math = 95.25;
+var biology = 82.5;
+var english = 83.24;
+var bangla = 81.75;
+var physics = 91.05;
+
+var totalMarks = math + biology + english + bangla + physics;
+// console.log(totalMarks);
+
+var averageMark = totalMarks / 5;
+averageMark = averageMark.toFixed(2);
+// console.log(averageMark);
+
+var number = 119;
+var divisor = 5;
+var remainder = number % divisor;
+console.log(remainder);
